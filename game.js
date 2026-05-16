7d87d61f9fbcb1de48cc5534058763413e0bd143
Jeremy Hayes <129910420+charlottearthub@users.noreply.github.com>
2026-05-16 15:04:50 -0400

    Load housing grid and nav checks

--- game.js
+++ game.js
@@ -3,11 +3,21 @@ mapStyles.rel = 'stylesheet';
 mapStyles.href = 'cyrex-mapnav.css';
 document.head.appendChild(mapStyles);
 
+const houseStyles = document.createElement('link');
+houseStyles.rel = 'stylesheet';
+houseStyles.href = 'cyrex-housing.css';
+document.head.appendChild(houseStyles);
+
 const gameScript = document.createElement('script');
 gameScript.src = 'game-fixed.js';
 gameScript.onload = function () {
   const mapScript = document.createElement('script');
   mapScript.src = 'cyrex-mapnav.js';
+  mapScript.onload = function () {
+    const houseScript = document.createElement('script');
+    houseScript.src = 'cyrex-nav-house.js';
+    document.body.appendChild(houseScript);
+  };
   document.body.appendChild(mapScript);
 };
 document.body.appendChild(gameScript);
