6a2d6cd4358add13dd340b1199580b303ea6718d
Jeremy Hayes <129910420+charlottearthub@users.noreply.github.com>
2026-05-16 14:57:41 -0400

    Load clickable map navigation

--- game.js
+++ game.js
@@ -1,3 +1,13 @@
-const script = document.createElement('script');
-script.src = 'game-fixed.js';
-document.body.appendChild(script);
+const mapStyles = document.createElement('link');
+mapStyles.rel = 'stylesheet';
+mapStyles.href = 'cyrex-mapnav.css';
+document.head.appendChild(mapStyles);
+
+const gameScript = document.createElement('script');
+gameScript.src = 'game-fixed.js';
+gameScript.onload = function () {
+  const mapScript = document.createElement('script');
+  mapScript.src = 'cyrex-mapnav.js';
+  document.body.appendChild(mapScript);
+};
+document.body.appendChild(gameScript);
